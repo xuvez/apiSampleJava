1864f2f408393d8326d82ea1a8ce0e04b0dcfe2a
palacgui <guillermo.palacio@adidas-group.com>
2019-09-13 12:21:41 +0000

    Jenkins X Kubernetes Workload

--- src/main/java/SampleController.java
+++ src/main/java/SampleController.java
@@ -11,7 +11,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello World2!";
     }
 
     public static void main(String[] args) throws Exception {
